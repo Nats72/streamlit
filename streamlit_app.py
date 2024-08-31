6a85d59d5dfb67490284c0c3462172d378d38de9
Nats72 <alphax72@gmail.com>
2024-09-01 00:14:26 +0900

    一旦完成

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,65 +20,16 @@ import google.ai.generativelanguage as glm
 # ページ設定
 st.set_page_config(page_title="Gemini Chatbot", page_icon=":whale:", layout="centered",initial_sidebar_state="auto",menu_items=None)
 
-# タイトル生成
-st.title("Geminiでチャットボット作ってみた")
-
-# ヘッダー&サブヘッダー
-st.header('へっだー')
-st.subheader('さぶへっだー')
-st.caption("きゃぷしょん")
-
 # ================================================================================
-# chat
+# Body
 # ================================================================================
-# # 定数定義
-# USER_NAME = "user"
-# ASSISTANT_NAME = "assistant"
-# MODEL_NAME = "model"
-
-# if "chat_log" not in st.session_state:
-#     st.session_state.chat_log = model.start_chat(history=[
-#         glm.Content(role=USER_NAME, parts=[glm.Part(text="あなたは優秀なAIアシスタントです。できるだけ簡潔に回答してください。")]),
-#         glm.Content(role=MODEL_NAME, parts=[glm.Part(text="わかりました。")])
-#     ])
-#     st.session_state.chat_log = []
-
-# user_msg = st.chat_input("ここにメッセージを入力")
-
-# if user_msg:
-#     # 以前のチャットログを表示
-#     for chat in st.session_state.chat_log:
-#         with st.chat_message(chat["name"]):
-#             st.write(chat["msg"])
-
-#     # 最新のメッセージを表示
-#     with st.chat_message(USER_NAME):
-#         st.write(user_msg)
-
-#     # アシスタントのメッセージを表示
-#     response = model.generate_content(user_msg)
-#     with st.chat_message(ASSISTANT_NAME):
-#         assistant_msg = response.text
-#         st.write(assistant_msg)
-
-#     # セッションにチャットログを追加
-#     st.session_state.chat_log.append({"name": USER_NAME, "msg": user_msg})
-#     st.session_state.chat_log.append({"name": ASSISTANT_NAME, "msg": assistant_msg})
-
-# # チャット履歴を表示
-# for message in st.session_state.chat_history:
-#     with st.chat_message(message["role"]):
-#         st.markdown(message["content"])
-
-
 # サイドバーにGeminiのAPIキーの入力欄を設ける
 with st.sidebar:
     gemini_api_key = st.text_input("Gemini API Key", key="chatbot_api_key", type="password")
     "[Get an Gemini API key](https://aistudio.google.com/app/apikey)"
-    "[View the source code](https://github.com/danishi/streamlit-gemini-chatbot)"
  
-st.title("✨ Gemini Chatbot")
-st.caption("🚀 A Streamlit chatbot powered by Gemini")
+st.title("Gemini Chatbot!")
+st.caption("サイドバーにAPIキーを入れてから、下の入力欄にテキストを入力して使ってください。")
  
 # セッションにチャット履歴がなければ初期化
 if "chat_history" not in st.session_state:
@@ -86,13 +37,15 @@ if "chat_history" not in st.session_state:
  
 # チャット履歴を表示
 for message in st.session_state.chat_history:
-    st.chat_message(message["role"]).write(message["content"])
- 
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+     
 # ユーザーの入力が送信された際に実行される処理
-if prompt := st.chat_input("How can I help you?"):
+if prompt := st.chat_input("ここにメッセージを入力してください。"):
  
     # APIキーのチェック
     if not gemini_api_key:
+        # APIキーがない場合はエラー
         st.info("Please add your [Gemini API key](https://aistudio.google.com/app/apikey) to continue.")
         st.stop()
  
@@ -103,7 +56,7 @@ if prompt := st.chat_input("How can I help you?"):
     # ユーザの入力をチャット履歴に追加し画面表示
     st.session_state.chat_history.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
- 
+
     # これまでの会話履歴を取得
     messages = []
     for message in st.session_state.chat_history:
