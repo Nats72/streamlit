e7df0a2c17b40b242b8efcc81466547e77c3c3db
Nats72 <alphax72@gmail.com>
2024-09-01 21:39:19 +0900

    テスト

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,115 +55,101 @@ def select_model():
     # モデル選択できるラジオボタンを設置
     models = ("Gemini")
     model = st.sidebar.radio("Choose a model:", models)
+
+    # モデル生成
     if model == "Gemini":
         st.session_state.model_name = "gemini-1.5-flash"
-        # モデル生成
         configurable_model = ChatGoogleGenerativeAI(
             temperature=temperature,
             model=st.session_state.model_name
         )
-        # configurable_model.configurable_fields(
-        #     model_name=ConfigurableField(id="model"),
-        #     default_key="gemini-1.5-flash"
-        # )
         return configurable_model
     
 def init_chain():
     st.session_state.llm = select_model()
     # ユーザーの入力をモデルに渡すためのテンプレートを定義
     prompt = ChatPromptTemplate.from_messages([
-        # *st.session_state.message_history,
         ("user", "{user_input}")
     ])
     # モデルからの返答から必要な情報を抽出
     output_parser = StrOutputParser()
-    return prompt | st.session_state.llm | output_parser
+    # LCELの書き方でプロンプトとモデル(llm)とoutput_parserを繋ぐ
+    init_chain = prompt | st.session_state.llm | output_parser
+    return init_chain
 
 def main():
     init_page()
     init_messages()
     chain = init_chain()
 
+    # タイトルとキャプション
     st.title("Gemini Chatbot!")
     st.caption("サイドバーにAPIキーを入れてから、下の入力欄にテキストを入力して使ってください。")
-    
-    # # セッションにチャット履歴がなければ初期化（message_historyを作成）
-    # if "message_history" not in st.session_state:
-    #     system_prompt = (
-    #         "Your purpose is to answer questions about specific documents only. "
-    #         "Please answer the user's questions based on what you know about the document. "
-    #         "If the question is outside scope of the document, please politely decline. "
-    #         "If you don't know the answer, say `I don't know`. "
-    #     )
-    #     st.session_state.message_history = [
-    #         # system promptを設定
-    #         {"role": "system", "content": system_prompt},
-    #         {"role": "assistant", "content": "何か気になることはありますか？"}
-    #         ]
     
     # チャット履歴を表示
     for message in st.session_state.message_history:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
-        
+            
     # ユーザーの入力が送信された際に実行される処理
     # chat_input()で、チャットUIでユーザの入力を待ち受けする
     if user_input := st.chat_input("ここにメッセージを入力してください。"):
     
-        # APIキーのチェック
-        gemini_api_key = os.getenv("GOOGLE_API_KEY")
-        if not gemini_api_key:
-            # APIキーがない場合はエラー
-            st.info("Please add your [Gemini API key](https://aistudio.google.com/app/apikey) to continue.")
-            st.stop()
+        # # APIキーのチェック
+        # gemini_api_key = os.getenv("GOOGLE_API_KEY")
+        # if not gemini_api_key:
+        #     # APIキーがない場合はエラー
+        #     st.info("Please add your [Gemini API key](https://aistudio.google.com/app/apikey) to continue.")
+        #     st.stop()
     
-        # モデルの設定
-        generation_config = {
-            "temperature": 0.9,  # 生成するテキストのランダム性を制御
-            "top_p": 1,          # 生成に使用するトークンの累積確率を制御
-            "top_k": 1,          # 生成に使用するトップkトークンを制御
-            "max_output_tokens": 2048,  # 最大出力トークン数を指定
-        }
-        # セーフティ設定
-        safety_settings = [
-            {
-                "category": "HARM_CATEGORY_HARASSMENT",  # ハラスメントに関する内容を制御
-                "threshold": "BLOCK_MEDIUM_AND_ABOVE"     # 中程度以上のハラスメントをブロック
-            },
-            {
-                "category": "HARM_CATEGORY_HATE_SPEECH",  # ヘイトスピーチに関する内容を制御
-                "threshold": "BLOCK_MEDIUM_AND_ABOVE"      # 中程度以上のヘイトスピーチをブロック
-            },
-            {
-                "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",  # 性的に露骨な内容を制御
-                "threshold": "BLOCK_MEDIUM_AND_ABOVE"            # 中程度以上の性的内容をブロック
-            },
-            {
-                "category": "HARM_CATEGORY_DANGEROUS_CONTENT",  # 危険な内容を制御
-                "threshold": "BLOCK_MEDIUM_AND_ABOVE"            # 中程度以上の危険な内容をブロック
-            }
-        ]
-        # モデルの初期化
-        genai.configure(api_key=gemini_api_key)
-        model = genai.GenerativeModel(
-            model_name='gemini-1.5-flash',
-            generation_config=generation_config,
-            safety_settings=safety_settings
-            )
+        # # モデルの設定
+        # generation_config = {
+        #     "temperature": 0.9,  # 生成するテキストのランダム性を制御
+        #     "top_p": 1,          # 生成に使用するトークンの累積確率を制御
+        #     "top_k": 1,          # 生成に使用するトップkトークンを制御
+        #     "max_output_tokens": 2048,  # 最大出力トークン数を指定
+        # }
+        # # セーフティ設定
+        # safety_settings = [
+        #     {
+        #         "category": "HARM_CATEGORY_HARASSMENT",  # ハラスメントに関する内容を制御
+        #         "threshold": "BLOCK_MEDIUM_AND_ABOVE"     # 中程度以上のハラスメントをブロック
+        #     },
+        #     {
+        #         "category": "HARM_CATEGORY_HATE_SPEECH",  # ヘイトスピーチに関する内容を制御
+        #         "threshold": "BLOCK_MEDIUM_AND_ABOVE"      # 中程度以上のヘイトスピーチをブロック
+        #     },
+        #     {
+        #         "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",  # 性的に露骨な内容を制御
+        #         "threshold": "BLOCK_MEDIUM_AND_ABOVE"            # 中程度以上の性的内容をブロック
+        #     },
+        #     {
+        #         "category": "HARM_CATEGORY_DANGEROUS_CONTENT",  # 危険な内容を制御
+        #         "threshold": "BLOCK_MEDIUM_AND_ABOVE"            # 中程度以上の危険な内容をブロック
+        #     }
+        # ]
+        # # モデルの初期化
+        # genai.configure(api_key=gemini_api_key)
+        # model = genai.GenerativeModel(
+        #     model_name='gemini-1.5-flash',
+        #     generation_config=generation_config,
+        #     safety_settings=safety_settings
+        #     )
     
         # ユーザの入力をチャット履歴に追加し画面表示
         # st.session_state.message_history.append({"role": "user", "content": user_input})
-        # st.chat_message("user").write(user_input)
+        st.chat_message("user").markdown(user_input)
 
         # これまでの会話履歴を取得
-        messages = []
-        for message in st.session_state.message_history:
-            messages.append(
-                {
-                    "role": message["role"] if message["role"] == "user" else "model",
-                    'parts': message["content"]
-                }
-            )
+        # messages = []
+        # for message in st.session_state.message_history:
+        #     messages.append(
+        #         {
+        #             "role": message["role"] if message["role"] == "user" else "model",
+        #             'parts': message["content"]
+        #         }
+        #     )
+        
     
         # Geminiへ問い合わせを行う
         # with st.spinner("Gemini is typing..."):
